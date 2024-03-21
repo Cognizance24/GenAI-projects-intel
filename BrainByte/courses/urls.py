787ac56a39bcfa319929106967978dd801951699
tayyab-ilyas <tayyabilyas963@gmail.com>
2024-03-21 17:11:07 +0530

    wip eduAi, home,login,signup

--- BrainByte/courses/urls.py
+++ BrainByte/courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.courses, name='courses'),
+    path('topic/<str:topic_name>/', views.show_topic, name='show_topic'),
 ]
--- BrainByte/courses/views.py
+++ BrainByte/courses/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def courses(request):
     render(request,'courses/playlist.html')
+
+def show_topic(request, topic_name):
+    context = {'topic_name': topic_name}
+    return render(request, 'courses/show_topic.html', context)
